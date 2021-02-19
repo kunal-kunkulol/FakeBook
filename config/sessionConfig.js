0dde37fac31f90dfb50e35d46be811eeec8927a0
Kunal Kunkulol <kunalkunkulol@gmail.com>
2021-02-19 20:57:15 +0530

    Fixed the cookie issue on production

--- config/sessionConfig.js
+++ config/sessionConfig.js
@@ -7,18 +7,19 @@ dotenv.config({ path: './config.env' });
 let RedisStore = require('connect-redis')(session);
 let redisClient = redis.createClient({
   host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD || ''
 });
 
 module.exports = session({
   secret: process.env.SESSION_SECRET,
   name: process.env.SESSION_NAME,
-  proxy: process.env.NODE_ENV === 'development',
+  proxy: process.env.NODE_ENV === 'production',
   cookie: {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: true,
-    maxAge: parseInt(process.env.SESSION_TIMEOUT)
+    maxAge: parseInt(process.env.SESSION_TIMEOUT),
+    signed: process.env.NODE_ENV === 'production'
   },
   store: new RedisStore({
     client: redisClient
